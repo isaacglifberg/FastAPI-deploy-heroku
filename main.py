8733d59cb6bf212c32b55d71f33ae5feb36b8f45
Isaac Glifberg <isaacglifberg@MacBook-Air-som-tillhor-Isaac.local>
2024-04-22 13:22:46 +0200

    changes to main

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://schoolsoft-support-bot-fe9057e7d85a.herokuapp.com", "http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
